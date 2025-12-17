4f075d063d721084d3f852bde2b6bbad74df4314
VinnSoga <vinnsogaharap@gmail.com>
2025-12-17 09:38:02 +0700

    owk2iwks

--- data.js
+++ data.js
@@ -2,9 +2,8 @@
 // Format: "slug": "link_sfile_mobi"
 const database_link = {
     "dosen-vs-mahasiswa": "https://sfile.mobi/kA8cVQH1PJ4",
-    "ebook-you-do-you-discovering-life-through-and-experiments-self-awareness": "https://sfile.mobi/kPEQZzVvITw",
     "zombie-infection-java": "https://sfile.mobi/QRUisaVnsCf",
-    "CONFIG-HC-V109": "https://sfile.mobi/nYBsv7LgY9A",
     "Panduan-FB-Meraih-Cuan": "https://sfile.mobi/yg21mtaFmJy",
     "1guy-1jar": "https://sfile.mobi/96QZ6301oXQ",
+    "Config-HC-V110": "https://sfile.mobi/yPWl8ayKmX0"
 };
